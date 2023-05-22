5b8b3916942527771c2b63d8d3f577d7600ff8c0
Victor Almeida Morais <vamdez123@gmail.com>
2023-05-22 15:49:48 -0300

    index api upgraded

--- api/index.js
+++ api/index.js
@@ -1,9 +1,12 @@
-const http = require("http");
 const URL = require("url");
 const fs = require("fs");
 const data = require("./urls.json");
 const path = require("path");
 const Site = require("./models/user");
+const express = require("express");
+
+const app = express();
+
 
 
 function writeFile(cb){
@@ -16,23 +19,23 @@ function writeFile(cb){
           )
 }
 
-
-http.createServer(async(req, res) => {
-    const {name, url, del} = URL.parse(req.url, true).query
-    res.writeHead(200, {"Access-Control-Allow-Origin":"*"}) //A Api precisa permitir que tenha acesso a outra porta,
-                                                            //nesse caso dei acesso a qualquer porta
+app.get("/",async(req, res)=>{
+    const {name, url, del} = URL.parse(req.url, true).query;
     if(!name || !url)
-        return res.end(JSON.stringify(data))
+        return res.end(JSON.stringify(data));
     if(del){
 
         await Site.destroy({
             where:{name,url}
-        })
-        
-        data.urls = data.urls.filter(item => String(item.url) !== String(url))
-        return writeFile((message =>{res.end(message)}))
+        });
+        data.urls = data.urls.filter(item => String(item.url) !== String(url));
+        return writeFile(message =>{res.end(message)})
     }
-    await Site.create({name, url})
-    data.urls.push({name, url})
-    return writeFile((message =>{res.end(message)}))
-}).listen(3000, () => console.log('API IS RUNNING'))
+    await Site.create({name, url});
+    data.urls.push({name, url});
+    return writeFile(message =>{res.end(message)});
+})
+
+app.listen(3001, ()=>{
+    console.log("API Runnig");
+});
